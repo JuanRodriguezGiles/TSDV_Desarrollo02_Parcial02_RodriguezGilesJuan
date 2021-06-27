9ceacedbeff86dba81256dae026ea7d562c4874f
Juan <juanrodriguezgiles@gmail.com>
2021-06-27 13:15:10 -0300

    Implemented splash screen scene

---
+++ LunarLander_RodriguezGilesJuan/Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using MonoBehaviourSingletonScript;
+using UnityEngine.SceneManagement;
+public enum Scenes
+{
+    SplashScreen,
+    MainMenu,
+    GamePlay
+}
+public class GameManager : MonoBehaviourSingleton<GameManager>
+{
+    public void LoadMainMenuScene()
+    {
+        SceneManager.LoadScene((int) Scenes.MainMenu);
+    }
+}
---
+++ LunarLander_RodriguezGilesJuan/Assets/Scripts/SplashScreen/SplashScreen.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+public class SplashScreen :MonoBehaviour
+{
+    public void LoadMainMenuScene()
+    {
+        GameManager.Get().LoadMainMenuScene();
+    }
+}
