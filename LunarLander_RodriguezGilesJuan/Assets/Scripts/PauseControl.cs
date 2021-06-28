58b4a3ae4970e3255ede25dd201d994b5ee5e8ce
Juan <juanrodriguezgiles@gmail.com>
2021-06-28 16:54:26 -0300

    Implemented pause functionality

---
+++ LunarLander_RodriguezGilesJuan/Assets/Scripts/PauseControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+public class PauseControl : MonoBehaviour
+{
+    public static bool gameIsPaused;
+    public void Pause()
+    {
+        gameIsPaused = !gameIsPaused;
+        PauseGame();
+    }
+    void PauseGame()
+    {
+        if (gameIsPaused)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
